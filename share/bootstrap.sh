e88d076c02d8fc2569e268318bb45c68411a21dd
Ryan Hansohn <hansohn@users.noreply.github.com>
2017-11-02 14:02:23 -0700

    bring repo up to date

--- share/bootstrap.sh
+++ share/bootstrap.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# setup basic features
 yum update -y && yum clean all;
 yum install -y curl git vim wget;
-sudo cp -f /vagrant/repo/motd /etc/motd;
+sudo cp -f /vagrant/share/motd /etc/motd;
 
+# customize
+# <-- add customizations here
+
+# exit
 echo "==> Welcome to Vagrant!";
 echo "==> run 'vagrant ssh' to login";
